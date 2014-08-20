5bc8da176f813eb051e745cc9f36395de6157373
ILYA <mortas.11@gmail.com>
2014-08-20 13:07:43 +0400

    Remove redundant escaping in EscapeString regexp

--- sphinxapi/__init__.py
+++ sphinxapi/__init__.py
@@ -1175,7 +1175,7 @@ class SphinxClient:
 
 
     def EscapeString(self, string):
-        return re.sub(r'([=\(\)|\-!@~\"&/\\\^\$=<])', r'\\\1', string)
+        return re.sub(r'([()|!@~"&/\\^$=<-])', r'\\\1', string)
 
 
     def FlushAttributes(self):
